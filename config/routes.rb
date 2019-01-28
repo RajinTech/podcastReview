4de107fc4936f843c18612ba9a12704ae374b27e
RajinTech <rajsglass@gmail.com>
2019-01-28 15:53:40 -0500

    made reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root 'podcasts#index'
+
+  resources :podcasts, only: [:index, :show]
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :podcasts, only: [:index, :show]
+      resources :reviews, only: [:index, :show]
+    end
+  end
+
 end
