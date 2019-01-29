0f748039117642aecad79a474466f58295437b12
Alex Yoon <yoon512@gmail.com>
2019-01-29 16:12:35 -0500

    tentative styling for PodcastIndexTile component

--- app/javascript/react/components/PodcastIndexTile.js
+++ app/javascript/react/components/PodcastIndexTile.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const PodcastIndexTile = props => {
-  let creators = props.creators.map((creator, index) => {
-    return(
-      <div key={index} className="podcast-detail">
-        {creator}
-      </div>
-    )
-  })
+const PodcastIndexTile = (props) => {
+  let creators = props.creators
+  let last_creator = creators.pop()
+  let creators_str
+  if (creators.length > 0) {
+    creators_str = `By ${creators.join(", ")} and ${last_creator}`
+  }
+  else {
+    creators_str = `By ${last_creator}`
+  }
 
   return (
-    <div className="panel small-4 columns">
+    <div className="panel small-4 columns content-tile">
       <div>
-        <Link to={`/podcasts/${props.id}`} className="podcast-title">{props.title}</Link>
-        {creators}
+        <h1>
+          <Link to={`/podcasts/${props.id}`}>{props.title}</Link>
+        </h1>
+        <h2>
+          {creators_str}
+        </h2>
       </div>
     </div>
   )
