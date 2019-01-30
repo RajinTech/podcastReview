cb5264f324281fb568d8fc033d8ca30ac5f1fefc
Anthony Rosato <AJRosato@gmail.com>
2019-01-30 17:09:08 -0500

    pending tests

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  pending "is valid with valid attributes" do
+
+  end
+
+  pending "is invalid without a value" do
+
+  end
+
+  pending "is invalid without a review" do
+
+  end
+
+  pending "is invalid without a user" do
+
+  end
 end
