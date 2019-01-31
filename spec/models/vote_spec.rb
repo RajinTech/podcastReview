3cf945305626e24c6ea069a64bbc40ed6720c7eb
Anthony Rosato <AJRosato@gmail.com>
2019-01-31 18:34:36 -0500

    fix rspec controller tests

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
   pending "is valid with valid attributes" do
-
+    expect(false).to eq (true)
   end
 
   pending "is invalid without a value" do
-
+    expect(false).to eq (true)
   end
 
   pending "is invalid without a review" do
-
+    expect(false).to eq (true)
   end
 
   pending "is invalid without a user" do
-
+    expect(false).to eq (true)
   end
 end
